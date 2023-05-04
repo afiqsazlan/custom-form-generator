b58f89cf7f2fcc10fd75c36a2e0d7fece2585b32
Afiq Sazlan <afiqsazlan@gmail.com>
2023-05-05 00:50:48 +0800

    Added /forms/create route for admin

--- routes/api/v1/admin.php
+++ routes/api/v1/admin.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Admin\FormController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-
+    Route::post('/forms/create', [FormController::class,'create']);
 });
